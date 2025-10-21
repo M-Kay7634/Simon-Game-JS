7a6f4ab0ffd25037b08390b26f6964c0cb7f00ec
M-Kay7634 <manishlloyd2807@gmail.com>
2025-10-21 18:09:49 +0530

    move a step back in commit history -> git reset HEAD~1

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 // created js file to save the javascript code.  i have add a button. 
 // created js file to save the javascript code. i have added a form.
 
+//Mistake , i want to reset or delete this line after add -> git reset script.js
